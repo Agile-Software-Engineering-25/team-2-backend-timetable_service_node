7cc87675b294213bc2301d2c43ed35cb97984b22
Julian <julian03.heine@web.de>
2025-11-13 20:58:23 +0100

    try 1

--- routers/v1/event.js
+++ routers/v1/event.js
@@ -31,11 +31,12 @@ router.post("/", requireRole("Area-1.Team-2.Read.Events"), async (req, res) => {
         logger.error(error)
         return res.status(400).json({ err: error })
     }
+    logger.info(req.body)
+
     try {
-        const event = new Event(req.body);
         logger.info(event)
         id = randomUUID()
-        const insertQuery = "INSERT INTO events (id, time, end_time, title, room_id, room_name,  study_group, lecturer_id, lecturer_name, type, module_name, comment) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        const insertQuery = "INSERT INTO events (id, time, end_time, title, room_id, room_name,  study_group, lecturer_id, lecturer_name, type, module_name, comment) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         await query(insertQuery, [id, event.time, event.endTime, event.title, event.room_id, event.room_name, event.studyGroup, event.lecturer_id, event.lecturer_name, event.type, event.module, event.comment ? event.comment : null]);
         event.id = id;
         res.status(201).json(event);
@@ -54,7 +55,7 @@ router.put("/:id", requireRole("Area-1.Team-2.Read.Events"), async (req, res) =>
         return res.status(400).json({ err: error })
     }
     try {
-        const insertQuery = "UPDATE events SET time = ?, end_time = ?, title = ?, room_id = ?, room_name = ?, study_group = ?, lecturer_id = ?,lecturer_name = ?, type = ?, module_name, comment = ? WHERE id = ?";
+        const insertQuery = "UPDATE events SET time = ?, end_time = ?, title = ?, room_id = ?, room_name = ?, study_group = ?, lecturer_id = ?,lecturer_name = ?, type = ?, module_name = ?, comment = ? WHERE id = ?";
         await query(insertQuery, [event.time, event.endTime, event.title, event.room_id, event.room_name, event.studyGroup, event.lecturer_id, event.lecturer_name, event.type, event.module, event.comment ? event.comment : null, eventId]);
         res.status(200).json(event);
     } catch (error) {
